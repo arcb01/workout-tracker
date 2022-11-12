221d25aa3ca2c3108d7e3480939cb3c5cfaea362
Arnau Castellano <arcascb2001@gmail.com>
2022-11-12 22:05:21 +0100

    Delete run.bat

--- run.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-python "D:\V\Projects\Python\Data Analysis\Notion gym workout\dashboard-v3\modules\run_retriever.py"
-python "D:\V\Projects\Python\Data Analysis\Notion gym workout\dashboard-v3\modules\data_processing.py"
-streamlit run "D:\V\Projects\Python\Data Analysis\Notion gym workout\dashboard-v3\workout_dashboard.py"
-pause
