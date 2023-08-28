27b2235f8415b201e196deb6e60bad253290c5fe
arnaucasbas <arcascb2001@gmail.com>
2023-08-28 22:21:11 +0200

    removed manual insertion of ids

--- testing_notion_api.py
+++ testing_notion_api.py
@@ -32,11 +32,12 @@ def workout_formatter(workout : dict):
     workout_location = workout["properties"]["Location"]["select"]["name"]
     workout_duration = workout["properties"]["Duration"]["number"]
 
-    return {"w_id" : workout["id"], # FIXME: Not sure if it is necessary now
+    return {
             "date" : workout_date,
             "intensity" : workout_intensity,
             "location" : workout_location,
             "duration" : workout_duration,
+            "exercises" : exercise_formatter(workout)
             }
 
 
@@ -54,13 +55,12 @@ def exercise_formatter(workout : dict):
 
     # Loop through all the exercises in the workout and format them
     for n in range(num_exercises):
-        workout_ex_id = workout_ex_db[n]["id"]
         workout_ex_name = workout_ex_db[n]["properties"]["Name"]["title"][0]["plain_text"]
         workout_ex_sets = workout_ex_db[n]["properties"]["Sets"]["number"]
         workout_ex_reps = workout_ex_db[n]["properties"]["Reps"]["number"]
         workout_ex_weight = workout_ex_db[n]["properties"]["Weight"]["number"]
-        workout_ex_data = { "w_id" : workout["id"], # FIXME: Not sure if it is necessary now
-                            "ex_name" : workout_ex_name,
+        workout_ex_data = { 
+                            "name" : workout_ex_name,
                             "sets" : workout_ex_sets,
                             "reps" : workout_ex_reps,
                             "weight" : workout_ex_weight,
@@ -70,34 +70,32 @@ def exercise_formatter(workout : dict):
 
     return workout_exercises
 
-
 def generate_workouts_data():
     """
     Generates a json file containing all the workouts data.
     The JSON file has 2 collections: Workout Sessions and Exercises.
     """
-
-    all_workouts = { "WRKT_SSNS" : [],
-                    "EXERCISES" : []}
     
+    # FIXME: This part is not ready
+    
+    workout_ssns = []
+    exercise_list = []
+
     wts_db_data = read_database(DATABASE_ID, headers)["results"]
     
     for workout in wts_db_data:
-        all_workouts["WRKT_SSNS"].append(workout_formatter(workout))
-        all_workouts["EXERCISES"].append(exercise_formatter(workout))
+        workout_ssns.append(workout_formatter(workout))
+        exercise_list.append(exercise_formatter(workout))
 
-    # Save the workout sessions collection
-    with open("./data/wrkt_ssn_data.json", "w", encoding="utf8") as f:
-        json.dump(all_workouts["WRKT_SSNS"], f, ensure_ascii=False)
+    data = {
+            "workout_sessions" : workout_ssns,
+            "exercises" : [item for sublist in exercise_list for item in sublist]
+            }
 
-    # Save the exercises collection
-    # FIXME: Not sure if this is necessary
-    unpacked_exercises = [item for sublist in all_workouts["EXERCISES"] for item in sublist]
-    with open("./data/ex_data.json", "w", encoding="utf8") as f:
-        json.dump(unpacked_exercises, f, ensure_ascii=False)
+    with open("./data/w_data.json", "w", encoding="utf8") as f:
+        json.dump(data, f, ensure_ascii=False)
 
     print("Succesfully workouts data generated.")
-
 
 
 if __name__ == "__main__":
