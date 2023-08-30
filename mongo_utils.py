2b572c2a8eeda5ac14eeb01b85ae6d8c5da17fdf
arnaucasbas <arcascb2001@gmail.com>
2023-08-30 16:00:08 +0200

    updated utils

--- mongo_utils.py
+++ mongo_utils.py
@@ -4,17 +4,17 @@ import pprint
 def create_collection(db: object, collection_name : str):
     """
     Creates a collection in the database and applies a validation schema to it.
-    NOTE: The validation schema is read from a json file with the same name as the collection.
     """
 
     # Create collection
     try:
         db.create_collection(collection_name)
+        print("Collection created succesfully""")
     except:
         print(f"Collection {collection_name} already exist")
 
+def apply_validation_schema(db: object, collection_name : str):
     # Read validation schema
-    """
     try:
         with open(f"./data/val_schemas/{collection_name}_val_schema.json", "r") as f:
             validation_schema = json.load(f)
@@ -23,19 +23,12 @@ def create_collection(db: object, collection_name : str):
 
     # Apply validation schema
     try:
+        # Delete validation schema if already exist
+        db.command({"collMod": collection_name, "validator": {}})
         db.command({"collMod": collection_name, "validator": validation_schema})
-    except:
-        print("Validator already exist")
-    """
-
-def show_validation_schema(db: object, collection_name : str):
-    """
-    Prints the validation schema of a collection.
-    """
-
-    validation_info = db[collection_name].options()["validator"]
-
-    pprint.pprint(validation_info)
+        print("Validation schema applied succesfully")
+    except Exception as e:
+        print(e)
 
 def delete_collection_data(db: object, collection_name : str):
     """
@@ -58,15 +51,10 @@ def delete_collection(db: object, collection_name : str):
         print("Collection already deleted")
 
 def show_collection(collection : object):
+    """
+    Shows all the documents in a collection.
+    """
     all_documents = collection.find()
 
     for document in all_documents:
         pprint.pprint(document)
-
-def empty_database(db : object):
-    """
-    Deletes all the data in the database.
-    """
-
-    for collection in db.list_collection_names():
-        delete_collection_data(db, collection)
