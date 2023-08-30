5ec7d6a7ac9086e4908319d96b16e9338fb7a622
arnaucasbas <arcascb2001@gmail.com>
2023-08-30 16:00:40 +0200

    new clean handler

--- mongo_handler.py
+++ mongo_handler.py
@@ -9,18 +9,22 @@ from pymongo import MongoClient
 from mongo_utils import *
 
 
-def insert_data_db(db):
-
+def insert_data_db(db : object):
+    """
+    Inserts data into the database.
+    """
+    
     # TODO: Updation phase: checking for duplicates
-    # FIXME: Validation schema not working
-
-    # Read clean data
+    
+    # 1. Read clean data
     with open("./data/workouts_data.json", "r") as f:
         w_data = json.load(f)
 
+    # Prepare database collections
     wsns_collection = db["wsns"]
     exs_collection = db["exs"]
 
+    # 2. Insert data
     for wokrout in w_data:
         workout_exercises = wokrout["exercises"]
         # Insert exercises into exs collection
@@ -30,29 +34,14 @@ def insert_data_db(db):
         wokrout["exercises"] = exs_ids.inserted_ids
         wsns_collection.insert_one(wokrout)
 
+
 if __name__ == "__main__":
 
     # Database connection
     client = MongoClient("mongodb://192.168.0.32:2717/")
     db = client["gym"]
-    exs_collection = db["exs"]
-    wsns_collection = db["wsns"]
 
-    print(exs_collection.count_documents({}))
-    print(wsns_collection.count_documents({}))
-
-    # 1. Create collection + FIXME validation schema
-    create_collection(db, "exs")
-    create_collection(db, "wsns")
-
-    # 2. Insert data
+    # Insert data
     insert_data_db(db)
-
-    print(exs_collection.count_documents({}))
-    print(wsns_collection.count_documents({}))
-
-    # 3. Show data
-    show_collection(wsns_collection)
-    #show_collection(exs_collection)
 
     client.close()
